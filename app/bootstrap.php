10f0f38e2f75f68d21cd18f9b4ae78a824f37cb6
Vít Kutný <vitkutny@gmail.com>
2013-07-28 00:39:49 +0200

    format

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,12 +9,17 @@ $configurator->createRobotLoader()
         ->addDirectory(__DIR__)
         ->addDirectory(__DIR__ . '/../libs')
         ->register();
-foreach (new DirectoryIterator(__DIR__) as $file){
-if($file->isDot() OR $file->getFilename()==='CMS') continue;
-if($file->isDir())
-$configurator->addConfig(__DIR__ . '/'.$file . '/config.neon');
+
+foreach (new DirectoryIterator(__DIR__) as $file) {
+    if ($file->isDot() OR $file->getFilename() === 'CMS') {
+        continue;
+    }
+    if ($file->isDir()) {
+        $configurator->addConfig(__DIR__ . '/' . $file . '/config.neon');
+    }
 }
 $configurator->addConfig(__DIR__ . '/CMS/config.neon');
+
 $container = $configurator->createContainer();
 
 return $container;
