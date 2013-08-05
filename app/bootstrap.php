61d92b27055b90cb0d34c2e710a75f6952989b9f
Vít Kutný <vitkutny@gmail.com>
2013-08-05 14:38:13 +0200

    dev

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,13 +1,12 @@
 <?php
 
-require_once __DIR__ . '/../libs/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 $configurator = new Nette\Configurator;
 //$configurator->setDebugMode(TRUE);
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->createRobotLoader()
         ->addDirectory(__DIR__)
-        ->addDirectory(__DIR__ . '/../libs')
         ->register();
 
 foreach (new DirectoryIterator(__DIR__) as $file) {
@@ -21,5 +20,4 @@ foreach (new DirectoryIterator(__DIR__) as $file) {
 $configurator->addConfig(__DIR__ . '/cms/config.neon');
 
 $container = $configurator->createContainer();
-
 $container->application->run();
