a0f098d8b690b09358705ca1480da89f6eb1b07e
Vít Kutný <vitkutny@gmail.com>
2013-08-09 09:56:23 +0200

    dev

---
+++ app/cms/Components/BaseControl.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CMS\Component;
+
+use Nette\Application\UI\Control;
+
+abstract class BaseControl extends Control {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
