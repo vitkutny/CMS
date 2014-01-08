70386ebe69ad1415b2ea2cdd184676184d30af28
Vít Kutný <vitkutny@outlook.com>
2014-01-08 09:58:33 +0100

    update

--- CMS/bootstrap.php
+++ CMS/bootstrap.php
@@ -5,18 +5,14 @@ $configurator = new Nette\Configurator;
 //$configurator->setDebugMode(TRUE);
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
-$configurator->createRobotLoader()
-        ->addDirectory(__DIR__)
-        ->addDirectory(__DIR__ . '/../vendor/others')
-        ->register();
 
 $configurator->addConfig(__DIR__ . '/config.neon');
-foreach (new DirectoryIterator(__DIR__ . '/../vendor/vitkutny') as $file) {
-    if ($file->isDot()) {
-        continue;
-    }
-    if ($file->isDir()) {
-        $configurator->addConfig(__DIR__ . "/../vendor/vitkutny/$file/CMS/config.neon");
+
+$directory = __DIR__ . '/../vendor/vitkutny';
+foreach (new DirectoryIterator($directory) as $node) {
+    $config = $directory . '/' . $node . '/CMS/config.neon';
+    if ($node->isDir() && !$node->isDot() && file_exists($config)) {
+        $configurator->addConfig($config);
     }
 }
 
--- www/index.php
+++ www/index.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once __DIR__ . '/../app/bootstrap.php';
+require_once __DIR__ . '/../CMS/bootstrap.php';
