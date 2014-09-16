484f3b059a878852d67bd976cfb606dfe783e5e7
Vít Kutný <vitkutny@outlook.com>
2014-09-16 22:30:56 +0200

    dev

--- private/bootstrap.php
+++ private/bootstrap.php
@@ -2,10 +2,9 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use WebEdit\Application;
+use WebEdit\Bootstrap;
 
-$configurator = new Application\Configurator;
+$configurator = new Bootstrap\Configurator;
 $configurator->setTempDirectory(__DIR__ . '/temp');
-$configurator->enableDebugger(__DIR__ . '/temp');
 $configurator->addConfig(__DIR__ . '/config.neon');
 return $configurator->createContainer();
