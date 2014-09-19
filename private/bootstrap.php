8973dd7aa9eca189b6e5fc175fcdb4d4450f33cb
Vít Kutný <vitkutny@outlook.com>
2014-09-19 09:21:46 +0200

    dev

--- private/bootstrap.php
+++ private/bootstrap.php
@@ -4,8 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use WebEdit\Bootstrap;
 
-$configurator = new Bootstrap\Configurator();
+$configurator = new Bootstrap\Configurator;
 return $configurator->setTempDirectory(__DIR__ . '/temp')
-    ->addConfig(__DIR__ . '/config.neon')
-    ->addConfig(__DIR__ . '/../vendor/config.json')
+    ->setVendorDirectory(__DIR__ . '/../vendor')
     ->createContainer();
