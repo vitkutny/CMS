af97a8f44d84462632382a499945b428222fd2a1
Vít Kutný <vitkutny@outlook.com>
2014-09-22 09:07:29 +0200

    dev

--- private/bootstrap.php
+++ private/bootstrap.php
@@ -5,6 +5,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use WebEdit\Bootstrap;
 
 $configurator = new Bootstrap\Configurator;
-return $configurator->setTempDirectory(__DIR__ . '/temp')
-    ->setVendorDirectory(__DIR__ . '/../vendor')
-    ->createContainer();
+return $configurator->createContainer();
