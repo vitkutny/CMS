60a0a606c7f54829386dddbcc8eab8ea0b8aaf2a
Vít Kutný <vitkutny@outlook.com>
2014-10-29 00:32:46 +0100

    reformat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,64 +1,64 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        bower: grunt.file.readJSON('./.bowerrc'),
-        webedit: {
-            parameters: {
-                publicTempDir: 'public/temp'
-            }
-        },
-        shell: {
-            default: {
-                command: [
-                    'chmod 777 private/temp',
-                    'chmod 777 public/temp',
-                    'bower install'
-                ].join('&&')
-            }
-        },
-        copy: {
-            default: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%=bower.directory%>/bootstrap/',
-                        src: ['fonts/*'],
-                        dest: '<%=webedit.parameters.publicTempDir%>/application/'
-                    }
-                ]
-            }
-        },
-        uglify: {
-            default: {
-                files: {
-                    '<%=webedit.parameters.publicTempDir%>/application/scripts/index.js': [
-                        '<%=bower.directory%>/jquery/dist/jquery.js',
-                        '<%=bower.directory%>/bootstrap/dist/js/bootstrap.js'
-                    ]
-                }
-            }
-        },
-        cssmin: {
-            default: {
-                files: {
-                    '<%=webedit.parameters.publicTempDir%>/application/styles/index.css': [
-                        '<%=bower.directory%>/bootstrap/dist/css/bootstrap.css',
-                        '<%=bower.directory%>/bootswatch/sandstone/bootstrap.css'
-                    ]
-                }
-            }
-        }
-    });
+	grunt.initConfig({
+		bower: grunt.file.readJSON('./.bowerrc'),
+		webedit: {
+			parameters: {
+				publicTempDir: 'public/temp'
+			}
+		},
+		shell: {
+			default: {
+				command: [
+					'chmod 777 private/temp',
+					'chmod 777 public/temp',
+					'bower install'
+				].join('&&')
+			}
+		},
+		copy: {
+			default: {
+				files: [
+					{
+						expand: true,
+						cwd: '<%=bower.directory%>/bootstrap/',
+						src: ['fonts/*'],
+						dest: '<%=webedit.parameters.publicTempDir%>/application/'
+					}
+				]
+			}
+		},
+		uglify: {
+			default: {
+				files: {
+					'<%=webedit.parameters.publicTempDir%>/application/scripts/index.js': [
+						'<%=bower.directory%>/jquery/dist/jquery.js',
+						'<%=bower.directory%>/bootstrap/dist/js/bootstrap.js'
+					]
+				}
+			}
+		},
+		cssmin: {
+			default: {
+				files: {
+					'<%=webedit.parameters.publicTempDir%>/application/styles/index.css': [
+						'<%=bower.directory%>/bootstrap/dist/css/bootstrap.css',
+						'<%=bower.directory%>/bootswatch/sandstone/bootstrap.css'
+					]
+				}
+			}
+		}
+	});
 
-    grunt.loadNpmTasks('grunt-shell');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('default', [
-        'shell',
-        'copy',
-        'uglify',
-        'cssmin'
-    ]);
+	grunt.registerTask('default', [
+		'shell',
+		'copy',
+		'uglify',
+		'cssmin'
+	]);
 
 };
--- webedit-base.sql
+++ webedit-base.sql
@@ -7,31 +7,35 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `menu_id` int(11) DEFAULT NULL,
-  `title` text NOT NULL,
-  `link` text,
-  `link_id` int(11) DEFAULT NULL,
-  `hidden` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	`id`      INT(11) NOT NULL AUTO_INCREMENT,
+	`menu_id` INT(11)    DEFAULT NULL,
+	`title`   TEXT    NOT NULL,
+	`link`    TEXT,
+	`link_id` INT(11)    DEFAULT NULL,
+	`hidden`  TINYINT(4) DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+	ENGINE =InnoDB
+	DEFAULT CHARSET =utf8;
 
 INSERT INTO `menu` (`id`, `menu_id`, `title`, `link`, `link_id`, `hidden`) VALUES
-(1,	NULL,	'Titulní strana',	':Home:Front:Presenter:view',	NULL,	NULL),
-(2,	NULL,	'Administrace',	':Home:Admin:Presenter:view',	NULL,	NULL);
+	(1, NULL, 'Titulní strana', ':Home:Front:Presenter:view', NULL, NULL),
+	(2, NULL, 'Administrace', ':Home:Admin:Presenter:view', NULL, NULL);
 
 DROP TABLE IF EXISTS `menu_group`;
 CREATE TABLE `menu_group` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `menu_id` int(11) NOT NULL,
-  `key` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `menu_id` (`menu_id`),
-  UNIQUE KEY `key` (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	`id`      INT(11)     NOT NULL AUTO_INCREMENT,
+	`menu_id` INT(11)     NOT NULL,
+	`key`     VARCHAR(32) NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `menu_id` (`menu_id`),
+	UNIQUE KEY `key` (`key`)
+)
+	ENGINE =InnoDB
+	DEFAULT CHARSET =utf8;
 
 INSERT INTO `menu_group` (`id`, `menu_id`, `key`) VALUES
-(1,	1,	'front'),
-(2,	2,	'admin');
+	(1, 1, 'front'),
+	(2, 2, 'admin');
 
 -- 2014-07-14 13:14:44
