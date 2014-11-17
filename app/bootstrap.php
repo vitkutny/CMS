6e94b0faf02c19ac7d8baa7f35cc3035d0b7cfb2
Vít Kutný <vitkutny@outlook.com>
2014-11-17 01:53:50 +0100

    dev

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,19 +5,4 @@ $configurator = new WebEdit\Config\Factory;
 $configurator->enableDebugger(__DIR__ . '/../temp');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->addConfig(__DIR__ . '/config.neon');
-$manager = new \Fuel\Alias\Manager;
-$manager->register();
-$manager->aliasPattern('Admin\*', 'WebEdit\\$1');
-$manager->aliasPattern('Front\*', 'WebEdit\\$1');
-//TODO: webedit/alias extension
-//default config
-//alias:
-//	prepend: TRUE
-//	class: []
-//	namespace: []
-//	pattern: []
-//let web/extension add patterns for webs
-//Fuel\Alias\Manager should be registered in every app request
-//implement alias cache
-//delete alias cache while extension compile
 return $configurator;
