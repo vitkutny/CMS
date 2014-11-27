dc01085b9a6355356337dff3c15130fea480294f
Vít Kutný <vitkutny@outlook.com>
2014-11-28 00:24:40 +0100

    dev

--- app/admin/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$configurator = require_once __DIR__ . '/../bootstrap.php';
-$configurator->addConfig(__DIR__ . '/config.neon');
-return $configurator;
--- app/admin/public/index.php
+++ app/admin/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$configurator = require_once __DIR__ . '/../bootstrap.php';
+$configurator = require_once __DIR__ . '/../../bootstrap.php';
 $container = $configurator->createContainer();
 $application = $container->getService('application');
 $application->run();
