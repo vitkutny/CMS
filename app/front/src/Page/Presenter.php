9a79fd7a9befd964f2dc87ba52699cd20ddfc1a9
Vít Kutný <vitkutny@outlook.com>
2015-02-06 01:39:09 +0100

    dev

--- app/front/src/Page/Presenter.php
+++ app/front/src/Page/Presenter.php
@@ -14,6 +14,7 @@ final class Presenter extends Ytnuk\Page\Presenter
 
 	public function renderView()
 	{
+		//TODO: this method should be in admin as preview, current admin renderView should be renderEdit
 		$this[Ytnuk\Web\Control::class][Ytnuk\Menu\Control::class]->setActive($this->page->menu);
 	}
 }
