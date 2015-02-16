59861101ec6191849550b98998a8c8300127c047
Vít Kutný <vitkutny@outlook.com>
2015-02-16 23:07:04 +0100

    dev

--- app/front/bootstrap.php
+++ app/front/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * @var Ytnuk\Config\Factory $configurator
+ */
 $configurator = require_once __DIR__ . '/../bootstrap.php';
 $configurator->enableDebugger(__DIR__ . '/temp');
 $configurator->setTempDirectory(__DIR__ . '/temp');
--- app/front/public/index.php
+++ app/front/public/index.php
@@ -1,8 +1,12 @@
 <?php
 
+/**
+ * @var Ytnuk\Config\Factory $configurator
+ */
 $configurator = require_once __DIR__ . '/../bootstrap.php';
-use Nette;
-
 $container = $configurator->createContainer();
+/**
+ * @var Nette\Application\Application $application
+ */
 $application = $container->getByType(Nette\Application\Application::class);
 $application->run();
