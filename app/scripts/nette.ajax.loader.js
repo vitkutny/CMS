66981fa0049ad44f948c8bdf6d30c367fce2e44c
Vít Kutný <vitkutny@icloud.com>
2015-11-14 14:34:57 +0100

    dev

--- app/scripts/nette.ajax.loader.js
+++ app/scripts/nette.ajax.loader.js
@@ -1,26 +1,21 @@
 //TODO: browser native loading ajax requests... instead of crappy overlays and spinners...
-
-(function ($) {
+//TODO: simulate native loading using dummy iframe in some browsers
+(function ($, element) {
 
 	$.nette.ext('loader', {
 		start: function (xhr, settings) {
-			if (!settings.nette) {
-				return;
+			if (!this.requests++) {
+				element.classList.add(this.class);
 			}
-			var loader = settings.nette.el.closest('[id^="snippet-"]') || $('#ajax-spinner');
-			loader = loader.add(settings.nette.el);
-			loader.attr('data-ajax-loader', '');
-			this.loaders.push(loader);
 		},
 		complete: function (xhr, status, settings) {
-			if (!settings.nette) {
-				return;
+			if (!--this.requests) {
+				element.classList.remove(this.class);
 			}
-			var loader = this.loaders.shift();
-			loader.removeAttr('data-ajax-loader');
 		}
 	}, {
-		loaders: []
+		requests: 0,
+		class: 'loading'
 	});
 
-})(window.jQuery);
+})(window.jQuery, document.documentElement);
