50d4905cd77d26a902b8b4761cd1893e9a92370c
Vít Kutný <vitkutny@icloud.com>
2015-11-20 01:53:20 +0100

    nette.ajax.scroll.js: instant scroll + modal container

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,6 +2,8 @@
 	$.nette.ext('init').linkSelector = 'a:not([data-toggle])';
 	$.nette.ext('init').formSelector = 'form';
 	$.nette.ext('init').buttonSelector = null;
+	$.nette.ext('scroll').speed = 0;
+	$.nette.ext('scroll').container = 'body, #modal';
 
 	$.nette.init();
 })(window.jQuery);
