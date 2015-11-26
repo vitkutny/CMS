159f4ca4bcd630beb0e5516f385c8d3648dc069b
Vít Kutný <vitkutny@icloud.com>
2015-11-26 23:47:14 +0100

    Vagrant: .profile cd "/vagrant"

--- server/bootstrap.user.sh
+++ server/bootstrap.user.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 php "/vagrant/app/run.php" migrations:reset
-grep -q "cd /vagrant" ~/.profile 2> /dev/null || echo "cd /vagrant" >> ~/.profile
+
+grep -q "cd /vagrant" ~/.profile 2> "/dev/null" || echo "cd /vagrant" >> ~/.profile
 cd "/vagrant"
+
 npm install
 grunt install
