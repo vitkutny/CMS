39fca927a78f8716aed7592a926a6545dcf1b072
Vít Kutný <vitkutny@icloud.com>
2015-11-26 01:32:22 +0100

    debug mode cookie

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ return call_user_func(
 		return call_user_func(
 			function (Composer\Autoload\ClassLoader $classLoader) : Nette\Configurator {
 				$configurator = new Nette\Configurator;
-				$configurator->setDebugMode(TRUE);
+				$configurator->setDebugMode('ytnuk@172.28.128.1');
 				$configurator->enableDebugger(__DIR__ . '/temp');
 				$configurator->setTempDirectory(__DIR__ . '/temp');
 				$configurator->addConfig(__DIR__ . '/../vendor/config.neon');
