999afb1f0550cd46b029adb3119ca7fdc6402651
Vít Kutný <vitkutny@icloud.com>
2015-12-10 00:48:02 +0100

    local.neon and local.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,12 +3,16 @@ return call_user_func(
 	function () : Nette\Configurator {
 		return call_user_func(
 			function (Composer\Autoload\ClassLoader $classLoader) : Nette\Configurator {
-				$configurator = new Nette\Configurator;
-				$configurator->setDebugMode('ytnuk@172.28.128.1');
+				if ( ! ($configurator = file_exists(__DIR__ . '/local.php') ? require_once __DIR__ . '/local.php' : NULL) instanceof Nette\Configurator) {
+					$configurator = new Nette\Configurator;
+				}
 				$configurator->enableDebugger(__DIR__ . '/temp');
 				$configurator->setTempDirectory(__DIR__ . '/temp');
 				$configurator->addConfig(__DIR__ . '/../vendor/config.neon');
 				$configurator->addConfig(__DIR__ . '/config.neon');
+				if (file_exists(__DIR__ . '/local.neon')) {
+					$configurator->addConfig(__DIR__ . '/local.neon');
+				}
 
 				return $configurator;
 			},
