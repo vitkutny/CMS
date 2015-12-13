08535a3c2243aae22020e8647356f9b7dab39f4d
Vít Kutný <vitkutny@icloud.com>
2015-12-13 01:20:25 +0100

    specify default scroll element, could be disabled on specific link using data-ajax-off=scroll

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,6 +4,7 @@
 	$.nette.ext('init').buttonSelector = null;
 	$.nette.ext('scroll').speed = 0;
 	$.nette.ext('scroll').container = 'body, #modal';
+	$.nette.ext('scroll').element = 'body, #modal > .modal-dialog';
 
 	$.nette.init();
 })(window.jQuery);
