ff0aa6c9385e59be7210efa681578fc8bbf7f2f2
Vít Kutný <vitkutny@icloud.com>
2015-12-17 09:22:26 +0100

    force redirect on ajax error

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,10 +1,18 @@
-(function ($) {
+(function ($, location) {
 	$.nette.ext('init').linkSelector = 'a:not([data-toggle])';
 	$.nette.ext('init').formSelector = 'form';
 	$.nette.ext('init').buttonSelector = null;
 	$.nette.ext('scroll').speed = 0;
 	$.nette.ext('scroll').container = 'body, #modal';
 	$.nette.ext('scroll').element = 'body, #modal > .modal-dialog';
+	$.nette.ext({
+		before: function (xhr, settings) {
+			xhr.settings = settings;
+		},
+		error: function (xhr) {
+			location.replace(xhr.settings.url);
+		}
+	});
 
 	$.nette.init();
-})(window.jQuery);
+})(window.jQuery, window.location);
