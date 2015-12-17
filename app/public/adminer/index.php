9cf028ae598314c0a6d1516f9a1dd6c94f3b9807
Vít Kutný <vitkutny@icloud.com>
2015-12-17 20:35:24 +0100

    use external adminer

--- app/public/adminer/index.php
+++ app/public/adminer/index.php
@@ -1,72 +1,69 @@
 <?php
-global $connection;
-$connection = call_user_func(
-	function () : Nextras\Dbal\Connection {
-		return call_user_func(
-			function (Nette\Configurator $configurator) : Nextras\Dbal\Connection {
+call_user_func(
+	function () {
+		$container = call_user_func(
+			function () : Nette\Di\Container {
 				return call_user_func(
-					function (Nette\DI\Container $container) use
-					(
-						$configurator
-					) : Nextras\Dbal\Connection {
-						if ( ! $configurator->isDebugMode()) {
-							call_user_func(
-								function (Nette\Application\Application $application) : Nette\Application\Application {
-									$application->run();
-									exit;
-								},
-								$container->getByType(Nette\Application\Application::class)
-							);
-						}
+					function (Nette\Configurator $configurator) : Nette\Di\Container {
+						return call_user_func(
+							function (Nette\DI\Container $container) use
+							(
+								$configurator
+							) : Nette\Di\Container {
+								if ( ! $configurator->isDebugMode()) {
+									call_user_func(
+										function (Nette\Application\Application $application) {
+											$application->run();
+											exit;
+										},
+										$container->getByType(Nette\Application\Application::class)
+									);
+								}
 
-						return $container->getByType(Nextras\Dbal\Connection::class);
+								return $container;
+							},
+							$configurator->createContainer()
+						);
 					},
-					$configurator->createContainer()
+					require_once __DIR__ . '/../../bootstrap.php'
 				);
+			}
+		);
+		call_user_func(
+			function (Nextras\Dbal\Connection $connection) {
+				global $adminer_config;
+				$adminer_config = $connection->getConfig() + [
+						'host' => 'localhost',
+						'user' => $username = posix_getpwuid(posix_geteuid())['name'] ?? NULL,
+						'password' => $username,
+					];
+				if ( ! isset($_GET['db'])) {
+					$_GET['db'] = posix_getpwuid(posix_geteuid())['name'] ?? NULL;
+				}
+				if (isset($adminer_config['driver'])) {
+					$_GET['username'] = $_GET[$adminer_config['driver']] = '';
+				}
 			},
-			require_once __DIR__ . '/../../bootstrap.php'
+			$container->getByType(Nextras\Dbal\Connection::class)
 		);
 	}
 );
-$config = $connection->getConfig();
-if ( ! isset($_GET['db'])) {
-	$_GET['db'] = posix_getpwuid(posix_geteuid())['name'] ?? NULL;
-}
-if (isset($config['driver'])) {
-	$_GET['username'] = $_GET[$config['driver']] = '';
-}
 function adminer_object()
 {
-	global $connection;
+	global $adminer_config;
 
-	return new class($connection)
+	return new class($adminer_config)
 		extends Adminer
 	{
-
-		/**
-		 * @var Nextras\Dbal\Connection
-		 */
-		private $connection;
 
 		/**
 		 * @var array
 		 */
 		private $config = [];
 
-		/**
-		 * @var Nextras\Dbal\Bridges\NetteTracy\ConnectionPanel
-		 */
-		private $panel;
-
-		public function __construct(Nextras\Dbal\Connection $connection)
+		public function __construct(array $config)
 		{
-			$this->connection = $connection;
-			$this->config = $connection->getConfig() + [
-					'host' => 'localhost',
-					'user' => $username = posix_getpwuid(posix_geteuid())['name'] ?? NULL,
-					'password' => $username,
-				];
-			$this->panel = Tracy\Debugger::getBar()->getPanel(Nextras\Dbal\Bridges\NetteTracy\ConnectionPanel::class);
+			$this->config = $config;
 		}
 
 		public function credentials()
@@ -77,42 +74,7 @@ function adminer_object()
 				$this->config['password'],
 			];
 		}
-
-		public function selectQuery(
-			$H,
-			$ih
-		) {
-			if ($this->panel) {
-				$this->panel->logQuery(
-					$this->connection,
-					$H,
-					new class(floatval($ih))
-						extends Nextras\Dbal\Result\Result
-					{
-
-						public function __construct(
-							$elapsedTime
-						) {
-							$elapsedTimeProperty = new ReflectionProperty(
-								parent::class,
-								'elapsedTime'
-							);
-							$elapsedTimeProperty->setAccessible(TRUE);
-							$elapsedTimeProperty->setValue(
-								$this,
-								$elapsedTime
-							);
-						}
-					}
-				);
-			}
-
-			return parent::selectQuery(
-				$H,
-				$ih
-			);
-		}
 	};
 }
 
-require_once __DIR__ . '/adminer-4.2.3.php';
+return require_once __DIR__ . '/../../../vendor/dg/adminer-custom/adminer.php';
