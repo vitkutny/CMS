a005eb3820bdc8a8e764bff3d74feaebfb3aaaa8
Vít Kutný <vitkutny@icloud.com>
2015-12-23 22:07:38 +0100

    migrations needs to be run after grunt install

--- server/bootstrap.user.sh
+++ server/bootstrap.user.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-php "/vagrant/application/run.php" migrations:reset
-
 grep -q "cd /vagrant" ~/.profile 2> "/dev/null" || echo "cd /vagrant" >> ~/.profile
 cd "/vagrant"
 
 npm install
 grunt install
+
+php "/vagrant/application/run.php" migrations:reset
