595d9e8326f4632bbf81bcf5eb9bf169e369f583
Vít Kutný <vitkutny@icloud.com>
2015-12-23 18:38:59 +0100

    use system /tmp directory

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,14 +3,17 @@ return call_user_func(
 	function () : Nette\Configurator {
 		return call_user_func(
 			function (Composer\Autoload\ClassLoader $classLoader) : Nette\Configurator {
-				if ( ! ($configurator = file_exists($local = __DIR__ . '/local.php') ? require_once $local : NULL) instanceof Nette\Configurator) {
+				if ( ! ($configurator = is_file($local = __DIR__ . '/local.php') ? require_once $local : NULL) instanceof Nette\Configurator) {
 					$configurator = new Nette\Configurator;
 				}
-				$configurator->enableDebugger(__DIR__ . '/temp');
-				$configurator->setTempDirectory(__DIR__ . '/temp');
+				if ( ! is_dir($tmp = '/tmp/ytnuk-sandbox')) {
+					@mkdir($tmp);
+				}
+				$configurator->enableDebugger($tmp);
+				$configurator->setTempDirectory($tmp);
 				$configurator->addConfig(__DIR__ . '/../vendor/config.neon');
 				$configurator->addConfig(__DIR__ . '/config.neon');
-				if (file_exists($local = __DIR__ . '/local.neon')) {
+				if (is_file($local = __DIR__ . '/local.neon')) {
 					$configurator->addConfig($local);
 				}
 
